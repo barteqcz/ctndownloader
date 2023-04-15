ce4f276f3c00c7b3e02fb46cbdd2a69bba3c36b1
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-04-15 14:31:05 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+import urllib.request
+import zipfile
+import io
+import os
+import shutil
+
+url = 'https://github.com/barteqcz/ctn/archive/refs/heads/main.zip' # URL to the release
+src_dir = "ctn-main"
+dst_dir = "."
+
+print("Updating the files...")
+
+# Download the zip file
+response = urllib.request.urlopen(url)
+zip_file = io.BytesIO(response.read())
+
+# Extract the zip file to the current directory
+with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+    zip_ref.extractall(os.getcwd())
+
+# Move the files to its parent directory
+for filename in os.listdir(src_dir):
+    src_file = os.path.join(src_dir, filename)
+    dst_file = os.path.join(dst_dir, filename)
+    shutil.move(src_file, dst_file)
+
+# Remove the source directory
+os.rmdir(src_dir)
+
+os.remove("README.md")
+os.remove("LICENSE.md")
+print("Updated the files successfully ^^")
+
+# Wait for the user to press a key
+input("Press any key to exit...")
